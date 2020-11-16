3148279d70e2a757bc56d73c635e80b248d2bddb
Mastagoon <iznilagiway@gmail.com>
2020-11-16 23:04:05 +0300

    skip&stop

---
+++ commands/stahp.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+const queue = new Map();
+
+const serverQueue = queue.get(message.guild.id);
+
+
+
+const isMasta = (id) => {
+    return id == "651501140386643969" ? true : false;
+}
+
+module.exports = {
+    name: "plyh",
+    description: "plyhs you",
+    execute(message, args) {
+        if (!message.member.voice.channel)
+          return isMasta(message.author.id) ?  message.channel.send("I am sorry master, you need to be in voice to stop the song.") : message.channel.send("cum voice chat first ya cunt.")
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    },
+};
