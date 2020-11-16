2f1ad64981f3d87d8293740f68d4a55ce200418c
Mastagoon <iznilagiway@gmail.com>
2020-11-16 23:09:27 +0300

    skip&stop

--- commands/skip.js
+++ commands/skip.js
@@ -7,12 +7,12 @@ const isMasta = (id) => {
     return id == "651501140386643969" ? true : false;
 }
 
-const serverQueue = queue.get(message.guild.id);
 
 module.exports = {
     name: "plyh",
     description: "plyhs you",
     execute(message, args) {
+        const serverQueue = queue.get(message.guild.id);
         if (!message.member.voice.channel)
           return isMasta(message.author.id) ?  message.channel.send("I am sorry master, you need to be in voice to stop the song.") : message.channel.send("cum voice chat first ya cunt.")
         serverQueue.songs = [];
--- commands/stahp.js
+++ commands/stahp.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const queue = new Map();
 
-const serverQueue = queue.get(message.guild.id);
 
 
 
@@ -14,6 +13,7 @@ module.exports = {
     name: "plyh",
     description: "plyhs you",
     execute(message, args) {
+        const serverQueue = queue.get(message.guild.id);
         if (!message.member.voice.channel)
           return isMasta(message.author.id) ?  message.channel.send("I am sorry master, you need to be in voice to stop the song.") : message.channel.send("cum voice chat first ya cunt.")
         serverQueue.songs = [];
